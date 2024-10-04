03f8ae553d6f900ecb72e60f6beaa434f158b91a
soundu2000 <soundaryah157@gmail.com>
2024-10-04 10:11:24 +0530

    Create_command

---
+++ student.sql
@@ -0,0 +1,33 @@
+create database studentdb;
+create table student1(ID INT(5), Name varchar(10));
+select * from Student1;
+select * from student1;
+alter table student1 add column marks int(10);
+select * from student1;
+alter table student1 add column email varchar(20);
+select * from student1;
+select * from student1;
+alter table student1 drop column marks;
+select * from student1;
+select * from student1;
+alter table student1 rename column name to email;
+select * from student1;
+alter table student1 rename column branch to course;
+select * from student1;
+alter table student1 rename column sec to dept;
+select * from student1;
+alter table student1 MODIFY id varchar(10);
+select * from student1;
+alter table student1 MODIFY email int(20);
+desc student1;
+select * from student1;
+alter table student1 MODIFY dept int(10);
+select * from student1;
+desc employee;
+desc student1;
+
+
+
+
+
+
